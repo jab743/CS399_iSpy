b9092e97fcd6f26f7f6a3005b57d07e3c08fb897
Ryan Buckingham <rsb93@nau.edu>
2015-11-01 15:21:05 -0700

    Layout Design

--- app/src/main/java/com/example/ryan/cs399ispy/About.java
+++ app/src/main/java/com/example/ryan/cs399ispy/About.java
@@ -1,21 +1,17 @@
 package com.example.ryan.cs399ispy;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class Home extends AppCompatActivity {
+public class About extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_about);
     }
 
     @Override
@@ -23,14 +19,6 @@ public class Home extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
-    }
-
-    public void buttonLiveClicked(View v) {
-        startActivity(new Intent(getApplicationContext(), Live.class));
-    }
-
-    public void buttonStillClicked(View v) {
-        startActivity(new Intent(getApplicationContext(), Still.class));
     }
 
     @Override
